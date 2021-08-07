40c53d8629e50e22892cfcc5203ef25ea0410cc1
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-07 10:06:29 +0800

    [ParkingLotStory2][Juan Carlos Salvador] refactor: Added changeable capacity of ParkingLot and Refactor checking if its full and if the ticket is recognizable

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,12 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    public static final int FULL_CAPACITY = 10;
-    private final Map<ParkingTicket, Car> parkedCars = new HashMap<>();
+    public static final int DEFAULT_CAPACITY = 10;
+    private final Map<ParkingTicket, Car> parkedCars;
+    private final int capacity;
+
+    public ParkingLot() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ParkingLot(int capacity) {
+        parkedCars = new HashMap<>();
+        this.capacity = capacity;
+    }
 
     public ParkingTicket park(Car car) {
 
-        if (FULL_CAPACITY == parkedCars.size()) {
+        if (isFullCapacity()) {
             throw new NoAvailablePositionException();
         }
 
@@ -20,12 +30,20 @@ public class ParkingLot {
 
     public Car fetch(ParkingTicket parkingTicket) {
 
-        if (!parkedCars.containsKey(parkingTicket)) {
+        if (!isRecognizedParkingTicket(parkingTicket)) {
             throw new UnrecognizedParkingTicketException();
         }
 
         Car parkedCar = parkedCars.get(parkingTicket);
         parkedCars.remove(parkingTicket);
         return parkedCar;
+    }
+
+    private boolean isFullCapacity() {
+        return capacity == parkedCars.size();
+    }
+
+    private boolean isRecognizedParkingTicket(ParkingTicket parkingTicket) {
+        return parkedCars.containsKey(parkingTicket);
     }
 }
