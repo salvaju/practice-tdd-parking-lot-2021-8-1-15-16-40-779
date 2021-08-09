810ae4de4747002fcb06df8cd93b1fccc3511a01
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-09 20:47:35 +0800

    [ParkingLotStory3][Juan Carlos Salvador] refactor: StandardParkingBoy extends ParkingBoy and remove unnesecarry code that is already in ParkingBoy

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -2,56 +2,26 @@ package com.parkinglot;
 
 import java.util.List;
 
-public class StandardParkingBoy {
-
-    private ParkingLot parkingLot;
-    private List<ParkingLot> parkingLots;
+public class StandardParkingBoy extends ParkingBoy{
 
     public StandardParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        super(parkingLot);
     }
 
     public StandardParkingBoy(List<ParkingLot> parkingLots) {
-        this.parkingLots = parkingLots;
+        super(parkingLots);
     }
 
+    @Override
     public ParkingTicket park(Car car) {
         if (!isParkingLotsNull()) {
-            return parkingLots.stream()
+            return getParkingLots().stream()
                     .filter(parkingLot -> !parkingLot.isFullCapacity())
                     .map(parkingLot -> parkingLot.park(car))
                     .findAny()
                     .orElseThrow(NoAvailablePositionException::new);
         }
 
-        return parkingLot.park(car);
-    }
-
-    public boolean isParkingLotsNull() {
-        return parkingLots == null;
-    }
-
-    public Car fetch(ParkingTicket parkingTicket) {
-        if (!isParkingLotsNull()) {
-            return parkingLots.stream()
-                    .filter(parkingLot -> parkingLot.isRecognizedParkingTicket(parkingTicket))
-                    .map(parkingLot -> parkingLot.fetch(parkingTicket))
-                    .findAny()
-                    .orElseThrow(UnrecognizedParkingTicketException::new);
-        }
-
-        return parkingLot.fetch(parkingTicket);
-    }
-
-    public boolean isParkedOnParkingLotNumber(ParkingTicket parkingTicket, int parkingLotNumber) {
-        return parkingLots.get(parkingLotNumber - 1).isRecognizedParkingTicket(parkingTicket);
-    }
-
-    public ParkingLot getParkingLot() {
-        return parkingLot;
-    }
-
-    public List<ParkingLot> getParkingLots() {
-        return parkingLots;
+        return getParkingLot().park(car);
     }
 }
